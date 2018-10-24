27b574c84051b2c968a4097458efc4873f3d9a02
Rita <42727192+Rita0719@users.noreply.github.com>
2018-10-24 16:33:46 +0800

    Add files via upload

--- assignment0/keywords.py
+++ assignment0/keywords.py
@@ -2,7 +2,19 @@ list1=['/Users/Rita/Desktop/assignment0/trade-wars-news1.txt','/Users/Rita/Deskt
 f=open(list1[0],'r')
 strings=f.read()
 words=strings.split()
-
-
+for word in words:
+    print('{}-{}times'.format(word,words.count(word)))
+    
 from collections import Counter
-print (Counter(words).most_common(15))
+print (Counter(words).most_common (15))
+list2=Counter(words).most_common ()
+
+headers = ['word','num']
+from csv import*
+import csv
+with open('list.csv','w',newline='')as f:
+ f_csv=csv.writer(f)
+ if(headers):
+    f_csv.writerow(headers)
+    if(list2):
+        f_csv.writerows(list2)
